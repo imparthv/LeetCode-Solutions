5dae172baea4b5b34c7f7743953f3b62b4414d79
imparthv <imparthvasdewani@gmail.com>
2023-08-24 22:27:08 +0530

    Solution to the problem 2235-Add Two Integers.

---
+++ 2235_Add_two_integers.py
@@ -0,0 +1,8 @@
+# 2235. Add two integers
+def sum(num1, num2):
+    return num1 + num2
+
+print("Enter the inputs:")
+inputNum1 = int(input())
+inputNum2 = int(input())
+print(sum(inputNum1, inputNum2))
