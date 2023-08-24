1f4b52b5dd3f0d3605d8593fffb3105a270b1ad1
imparthv <imparthvasdewani@gmail.com>
2023-08-24 21:52:36 +0530

    Solution to the problem 50-Pow(x, n).

---
+++ 50_power_x_n_complete.py
@@ -0,0 +1,8 @@
+# 50. Pow(x, n)
+def myPow(x, n):
+    return float(x ** n)
+
+print("Enter base and exponent:")
+base_number = float(input())
+exponent_number =  int(input()) 
+print(myPow(base_number, exponent_number))
