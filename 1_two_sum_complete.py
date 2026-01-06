0ab39dc00c2282cbc54e30f1101c433f9648caf5
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-06 12:43:21 +0530

    Modified solution with optimization than its precursor

--- 1_two_sum_complete.py
+++ 1_two_sum_complete.py
@@ -1,14 +1,12 @@
 # 1. Two Sum
 def twoSum(nums, target):
     ans_list = []
-    for val1 in range(0, len(nums)):
-        for val2 in range(1, len(nums)):
-            if val1 != val2 and nums[val1] + nums[val2] == target:
-                ans_list.append(val1)
-                ans_list.append(val2)
-                return ans_list
-input_list =[3, 3]
-print("Enter target:")
-target_num = int(input())
-output_list = twoSum(input_list, target_num)
-print(output_list)
+    for index_val in range(0,  len(nums)):
+        target_val = target - nums[index_val]
+        if target_val in nums[index_val+1:]:
+            ans_list.append(index_val)
+            ans_list.append(nums.index(target_val, index_val+1))
+            break
+    return ans_list
+
+print(twoSum([3,4,5,6], 10))
